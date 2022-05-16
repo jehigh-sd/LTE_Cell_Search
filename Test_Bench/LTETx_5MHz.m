b3016462b66775de1f0feb655e93e6d3f9938154
Mahesh Valavala <vvalavala@ucsd.edu>
2022-05-15 21:32:58 -0700

    Add files via upload

---
+++ Test_Bench/LTETx_5MHz.m
@@ -0,0 +1,19 @@
+ %LTE_Wfm2 = load('IQ_6RB_QPSK_CellID11.mat');
+ 
+
+ fs = 7.68e6;
+ 
+ %enable this for Sine Wfm
+ %sw = dsp.SineWave;
+ sw = LTE_Wfm_5M;
+ txWaveform = sw();  
+ 
+ tx = sdrtx('Pluto');
+ %tx.CenterFrequency = 2.4e9;
+ tx.CenterFrequency = 860e6;
+ tx.BasebandSampleRate = fs;
+ tx.Gain = -5;
+ transmitRepeat(tx,txWaveform.waveform);
+ 
+ %release(tx);
+  
